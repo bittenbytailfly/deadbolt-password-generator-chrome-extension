77be7b20c2889f17e9553780edd1c86982abbf50
Ed Carter <bittenbytailfly@server.fake>
2013-01-04 19:43:48 +0000

    Added licensing information

--- js/options.js
+++ js/options.js
@@ -1,4 +1,20 @@
-﻿window.addEventListener('message', function (event) {
+﻿//    This file is part of Deadbolt Password Generator.
+//
+//    Deadbolt Password Generator is free software: you can redistribute 
+//    it and/or modify it under the terms of the GNU General Public 
+//    License as published by the Free Software Foundation, either 
+//    version 3 of the License, or (at your option) any later version.
+//
+//    Deadbolt Password Generator is distributed in the hope that it 
+//    will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+//    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+//    See the GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Deadbolt Password Generator.  If not, see 
+//    <http://www.gnu.org/licenses/>.
+
+window.addEventListener('message', function (event) {
     var command = event.data.command;
     switch (command) {
         case 'loaded':
